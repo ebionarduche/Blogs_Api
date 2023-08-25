24999574c0644e46fe6f46d7fa54ba5782a0d358
Ebio Narduche <ebionarduche77@gmail.com>
2023-08-25 16:19:45 -0300

    Requisito 01 e 02 ok

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ * 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { 
+      allowNull: false,
+      autoIncrement: true, 
+      primaryKey: true, 
+      type: DataTypes.INTEGER 
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User; // Corrigido de Employee para User
+};
